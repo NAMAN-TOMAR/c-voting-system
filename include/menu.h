60526c1ec06c0d5d51c4b382de08395e89d7282f
NAMAN-TOMAR <namantomar.in@gmail.com>
2025-11-30 17:43:10 +0530

    Add menu header file with menu function declarations

--- include/menu.h
+++ include/menu.h
@@ -1,1 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+/* Show the main menu (Admin/User/Exit) */
+void showMainMenu();
+
+/* Admin specific menu (add, list, results) */
+void showAdminMenu();
+
+/* User specific menu (list, vote) */
+void showUserMenu();
+
+#endif
 
