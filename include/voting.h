185ab828d0410774eb3388bd590e64e17a1b49f1
NAMAN-TOMAR <namantomar.in@gmail.com>
2025-11-30 17:42:52 +0530

    Update voting.h

--- include/voting.h
+++ include/voting.h
@@ -1,1 +1,27 @@
+#ifndef VOTING_H
+#define VOTING_H
+
+#define MAX_CANDIDATES 10
+
+typedef struct {
+    char name[50];
+    int votes;
+} Candidate;
+
+/* Initialize candidate list */
+void initCandidates();
+
+/* Add a new candidate */
+void addCandidate();
+
+/* List all candidates (for both admin and user) */
+void listCandidates();
+
+/* Cast a vote for a selected candidate */
+void castVote();
+
+/* Show all results and declare winner */
+void showResults();
+
+#endif
 
