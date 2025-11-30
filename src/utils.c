c9308840ac971720fcdf4e3e369dd33fb37644d0
NAMAN-TOMAR <namantomar.in@gmail.com>
2025-11-30 17:43:44 +0530

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "utils.h"
+
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        /* discard */
+    }
+}
+
+int readInt() {
+    int value;
+    int result;
+
+    result = scanf("%d", &value);
+    if (result != 1) {
+        /* invalid input, clear buffer and set default */
+        clearInputBuffer();
+        return -1;
+    }
+    return value;
+}
 
